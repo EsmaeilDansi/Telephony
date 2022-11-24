b7c11f05f43465dc3a0f383c8b2c1f63ad76f1d5
Edansi <e.dansi.111@gmail.com>
2022-11-24 09:26:14 +0330

    add connection

--- android/src/main/kotlin/com/shounakmulay/telephony/TelephonyPlugin.kt
+++ android/src/main/kotlin/com/shounakmulay/telephony/TelephonyPlugin.kt
@@ -12,6 +12,7 @@ import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.*
 import com.shounakmulay.telephony.sms.FlutterHandler
+import com.shounakmulay.telephony.sms.ConnectivityReceiver
 import android.util.Log
 import android.content.Intent
 import android.net.ConnectivityManager
