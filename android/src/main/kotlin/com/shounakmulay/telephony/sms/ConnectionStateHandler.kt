bde0d148f5e0cfb467c355e43946979420954415
Edansi <e.dansi.111@gmail.com>
2022-11-27 12:27:54 +0330

    some bug fix.

--- android/src/main/kotlin/com/shounakmulay/telephony/sms/ConnectionStateHandler.kt
+++ android/src/main/kotlin/com/shounakmulay/telephony/sms/ConnectionStateHandler.kt
@@ -19,49 +19,37 @@ import android.util.Log
 
 
 class ConnectionStateHandler(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    private var networkStateEventChannel: EventChannel = EventChannel(binding.binaryMessenger, "NETWORK_STATE_STREAM")
 
     init {
-        ConnectivityReceiver.connectivityReceiverListener = this
-        ConnectivityReceiver.ConnectivityReceiverListener {
-            override fun onNetworkConnectionChanged(isConnected: Boolean) {
+        networkStateEventChannel.setStreamHandler(object : EventChannel.StreamHandler {
+            private lateinit var receiver: ConnectivityReceiver
+
+            override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+                receiver = object : PhoneStateReceiver() {
+                    override fun onReceive(context: Context?, intent: Intent?) {
+                        super.onReceive(context, intent)
+                        Log.e("conneection state", "networkkkkkkkk")
+                        events?.success(receiver.status.toString())
+                    }
+                }
+
                 Log.e("connection sateeeeeeeee", "change  connection")
                 binding.applicationContext.registerReceiver(
                         receiver,
                         IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
                 )
-
             }
 
-//            override fun onResume() {
-//                super.onResume()
-//                ConnectivityReceiver.connectivityReceiverListener = this
-//            }
-        }
-//        phoneStateEventChannel.setStreamHandler(object : EventChannel.StreamHandler {
-//            private lateinit var receiver: PhoneStateReceiver
-//
-//            override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-//                receiver = object : PhoneStateReceiver() {
-//                    override fun onReceive(context: Context?, intent: Intent?) {
-//                        super.onReceive(context, intent)
-//                        Log.e("phonestate", "incoming call.")
-//                        events?.success(receiver.status.toString())
-//                    }
-//                }
-//
-//                binding.applicationContext.registerReceiver(
-//                        receiver,
-//                        IntentFilter(TelephonyManager.ACTION_PHONE_STATE_CHANGED)
-//                )
-//            }
-//
-//            override fun onCancel(arguments: Any?) {
-//                binding.applicationContext.unregisterReceiver(receiver)
-//            }
-//        })
+            override fun onCancel(arguments: Any?) {
+                binding.applicationContext.unregisterReceiver(receiver)
+            }
+        })
     }
 
     fun dispose() {
-//        phoneStateEventChannel.setStreamHandler(null)
+        networkStateEventChannel.setStreamHandler(null)
     }
+
+
 }
